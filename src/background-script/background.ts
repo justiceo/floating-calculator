2960756047d97a11bb43d926cb56e58c351322c6
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-03-14 12:06:40 -0700

    Update uninstall feedback form

--- src/background-script/background.ts
+++ src/background-script/background.ts
@@ -3,7 +3,7 @@ import { ContextMenu } from "./context-menus";
 
 new ContextMenu().init();
 
-const uninstallUrl = "https://forms.gle/oRACFiJQCZW77Ssu8";
+const uninstallUrl = "https://forms.gle/iE1DgamFFEy2g2fDA";
 const welcomeUrl = chrome.runtime.getURL("welcome/welcome.html");
 
 const onInstalled = (details: chrome.runtime.InstalledDetails) => {
