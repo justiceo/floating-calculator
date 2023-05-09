299cb35d253f64bba115a280f966fc68b350f43a
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-05-08 20:03:47 -0700

    Update homepage url, version, add inline-feedback

--- src/content-script/search-engine.ts
+++ src/content-script/search-engine.ts
@@ -60,7 +60,29 @@ export function getEngineConfig() {
                     a.parentElement?.replaceChild(document.createTextNode(a.textContent!), a);
                 });                
             },
-        }
+        },
+        Bing: {
+            name: "Bing",
+            selector: ".b_ans.b_top",            
+            url: () => chrome.runtime.getURL("content-script/bing-calculator.html"),
+            applyCss: () => {
+                const style = document.createElement("style");
+                style.textContent = `                    
+                    body.srp {
+                        overflow-x: hidden;
+                        overflow-y: hidden;
+                        --center-width: 350px;
+                        --center-abs-margin: 0;
+                    }                                    
+                `;
+                document.body.appendChild(style);
+
+                // Remove links to avoid navigating away.
+                document.body.querySelectorAll("span[role=link]").forEach(a => {
+                    a.parentElement?.replaceChild(document.createTextNode(a.textContent!), a);
+                });
+            },
+        },
     }
     return config[engine];
 }
