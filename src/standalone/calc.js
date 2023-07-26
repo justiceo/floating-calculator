83a203f9b445d693545176214799847513c9302f
Justice Ogbonna <justiceo@users.noreply.github.com>
2023-07-26 22:31:38 +0100

    Integrate insect library

--- src/standalone/calc.js
+++ src/standalone/calc.js
@@ -1,14 +1,34 @@
 
+var insectEnv = Insect.initialEnvironment;
+var clearCommands = ["clear", "cls", "quit", "exit"];
+
+function evaluateInput(line) {
+    console.warn("evaluating line: ", line);
+    var res = Insect.repl(Insect.fmtPlain)(insectEnv)(line);
+    if(res.msgType === 'error') {
+        console.error("insect:", res.msg);
+        return "";
+    }
+    console.info("insect res: ", res)
+    insectEnv = res.newEnv;
+    return res.msg;
+}
 
 function handleClick(text) {
     console.log("User clicked", text);
     let input = document.querySelector("input#text-input");
+    let pretext = document.querySelector("span#pretext");
     switch(text) {
         case "=":
-            // eval
-            // update input value to answer
-            // update results label to prompt
-            // update history
+            // Todo: update history
+            let res = evaluateInput(input.value)
+            if(res) {
+                pretext.innerText = input.value + " =";
+                input.value = res;
+                input.classList.remove("border-danger");
+            } else {
+                input.classList.add("border-danger");
+            }
             break;
         case "AC":
             input.value = "";
