21c592348258162cb21b70267521db8f3b937346
Justice Ogbonna <justiceo@users.noreply.github.com>
2024-01-16 17:38:02 +0100

    Enable options page with a pletora of configs

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,77 @@ export const sentryDsn =
 export const measurementId = "G-ZCZLZLYH36";
 export const gaApiSecret = "UIXmDH2iRxaZPMd1S_UUww";
 export const uninstallUrl = "https://forms.gle/iE1DgamFFEy2g2fDA";
-export const configOptions: Config[] = [];
+export const configOptions: Config[] = [
+  {
+    id: "default-width",
+    type: "range",
+    title: "optionDefaultWidth",
+    description: "optionDefaultWidthDesc",
+    default_value: 40,
+    min: "20",
+    max: "90",
+  },
+  {
+    id: "default-height",
+    type: "range",
+    title: "optionDefaultHeight",
+    description: "optionDefaultHeightDesc",
+    default_value: 70,
+    min: "20",
+    max: "95",
+  },
+  {
+    id: "blocked-sites",
+    type: "textarea",
+    title: "optionBlockedSites",
+    description: "optionBlockedSitesDesc",
+    default_value: "",
+  },
+  {
+    id: "enable-fractions",
+    type: "switch",
+    title: "optionEnableFractions",
+    description: "optionEnableFractionsDesc",
+    default_value: false,
+  },
+  {
+    id: "answer-precision",
+    type: "range",
+    title: "optionAnswerPrecision",
+    description: "optionAnswerPrecisionDesc",
+    default_value: 6,
+    min: "1",
+    max: "10",
+  },
+  {
+    id: "default-to-basic",
+    type: "switch",
+    title: "optionDefaultToBasic",
+    description: "optionDefaultToBasicDesc",
+    default_value: false,
+  },
+  {
+    id: "enable-minimize",
+    type: "switch",
+    title: "optionEnableMinimize",
+    description: "optionEnableMinimizeDesc",
+    default_value: false,
+  },
+  {
+    id: "enable-dark-mode",
+    type: "switch",
+    title: "optionEnableDarkMode",
+    description: "optionEnableDarkModeDesc",
+    default_value: false,
+  },
+  {
+    id: "use-comma-decimals",
+    type: "switch",
+    title: "optionUseCommaForDecimals",
+    description: "optionUseCommaForDecimalsDesc",
+    default_value: false,
+  },
+];
 
 export const contextMenus: MenuItem[] = [
   {
